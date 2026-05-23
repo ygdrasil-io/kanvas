b9dab54b9fa2a7fba90620731bd3c9247d2c67c3
ygdrasil-io <alexandre.mommers@gmail.com>
2026-05-24 01:17:43 +0200

    Add pure Kotlin BMP codec

--- codec-bmp-kotlin/build.gradle.kts
+++ codec-bmp-kotlin/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(project(":kanvas-skia"))
     implementation(project(":codec-core"))
     implementation(project(":codec-common"))
+
+    testImplementation(project(":math"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
