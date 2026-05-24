8b8cb0081ca2a12aff71012e49568498433612a6
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2026-05-24 14:17:03 +0200

    Implement surface subset snapshots (#870)

--- skia-integration-tests/src/test/kotlin/org/skia/tests/SurfaceUnderdrawTest.kt
+++ skia-integration-tests/src/test/kotlin/org/skia/tests/SurfaceUnderdrawTest.kt
@@ -1,14 +1,12 @@
 package org.skia.tests
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.skia.testing.TestUtils
 
-@Disabled("STUB.SURFACE_SNAPSHOT_SUBSET: SkSurface.makeImageSnapshot(SkIRect) not yet implemented")
 class SurfaceUnderdrawTest {
 
     @Test
-    fun `SurfaceUnderdrawGM surface_underdraw placeholder`() {
+    fun `SurfaceUnderdrawGM renders subset snapshot path`() {
         TestUtils.runGmTest(SurfaceUnderdrawGM())
     }
 }
