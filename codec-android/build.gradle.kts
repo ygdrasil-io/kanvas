558d409f3f4616fa3c23867e6e6e308c5520133f
ygdrasil-io <alexandre.mommers@gmail.com>
2026-05-24 13:47:42 +0200

    Add Android codec Kotlin bundle tests

--- codec-android/build.gradle.kts
+++ codec-android/build.gradle.kts
@@ -7,4 +7,10 @@ dependencies {
     implementation(project(":math"))
     implementation(project(":kanvas-skia"))
     implementation(project(":codec-core"))
+
+    testImplementation(project(":codec-all-kotlin"))
+    testImplementation(project(":codec-test-fixtures"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
