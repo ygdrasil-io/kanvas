d0031b8d973ff416a584fe51432c3d6d92cd74aa
ygdrasil-io <alexandre.mommers@gmail.com>
2026-06-30 01:10:37 +0200

    feat(integration-tests/svg): add dependencies for SVG integration tests

--- integration-tests/svg/build.gradle.kts
+++ integration-tests/svg/build.gradle.kts
@@ -5,4 +5,22 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(project(":kanvas"))
+    implementation("io.ygdrasil:wgpu4k-toolkit:0.2.0-SNAPSHOT")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+sourceSets {
+    test {
+        resources.srcDir("src/main/resources")
+        resources.srcDir("src/test/resources")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
