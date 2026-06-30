86054acaa2c55b74c514f714fadd29b30baaf92e
ygdrasil-io <alexandre.mommers@gmail.com>
2026-06-30 03:12:37 +0200

    fix: add missing opentest4j dependency for GpuAvailability

--- integration-tests/test-utils/build.gradle.kts
+++ integration-tests/test-utils/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":kanvas"))
     implementation("io.ygdrasil:wgpu4k-toolkit:0.2.0-SNAPSHOT")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.opentest4j:opentest4j:1.3.0")
 }
 
 sourceSets {
