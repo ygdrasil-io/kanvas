98632b07d5b4dd2bfb2b68ee1b2ab1dca5cb5077
ygdrasil-io <alexandre.mommers@gmail.com>
2026-07-03 01:55:00 +0200

    test(text): add SmokeTextGm to verify drawString end-to-end

---
+++ integration-tests/skia/src/test/kotlin/org/graphiks/kanvas/skia/gm/text/SmokeTextGm.kt
@@ -0,0 +1,27 @@
+package org.graphiks.kanvas.skia.gm.text
+
+import org.graphiks.kanvas.paint.Paint
+import org.graphiks.kanvas.skia.GmCanvas
+import org.graphiks.kanvas.skia.RenderFamily
+import org.graphiks.kanvas.skia.SkiaGm
+import org.graphiks.kanvas.text.Font
+import org.graphiks.kanvas.text.Typefaces
+import org.graphiks.kanvas.types.Color
+
+class SmokeTextGm : SkiaGm {
+    override val name = "smoke_text"
+    override val renderFamily = RenderFamily.TEXT
+    override val minSimilarity = 0.0
+    override val width = 200
+    override val height = 100
+
+    private val font = Font(
+        typeface = Typefaces.fromResource("fonts/LiberationSans-Regular.ttf")!!,
+        size = 24f,
+    )
+
+    override fun draw(canvas: GmCanvas, width: Int, height: Int) {
+        canvas.drawString("Hello Kanvas", 10f, 40f, font, Paint(color = Color.BLACK))
+        canvas.drawString("GM Text", 10f, 80f, font, Paint(color = Color.RED))
+    }
+}
