cefb77cfdf9a32ad601f49e7b7e6ff41dbfe8322
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2026-07-05 14:48:56 +0200

    gm: migrate FontCacheGM + FontRegenGM, remove Yuv420OddDimGM (#1978)
    * gm: migrate 9 PictureRecorder GMs
    
    * gm: remove spurious BackdropHelpers.kt from old test directory
    
    * gm: remove Yuv420OddDimGM (Ganesh-only, intractable)
    
    * gm: migrate FontCacheGM + FontRegenGM

---
+++ integration-tests/skia/src/test/kotlin/org/graphiks/kanvas/skia/gm/text/FontCacheGm.kt
@@ -0,0 +1,64 @@
+package org.graphiks.kanvas.skia.gm.text
+
+import org.graphiks.kanvas.paint.Paint
+import org.graphiks.kanvas.skia.GmCanvas
+import org.graphiks.kanvas.skia.RenderFamily
+import org.graphiks.kanvas.skia.SkiaGm
+import org.graphiks.kanvas.text.Font
+import org.graphiks.kanvas.text.Typefaces
+
+/**
+ * Port of Skia's `gm/fontcache.cpp::FontCacheGM`.
+ * Exercises glyph rendering with multiple sizes and subpixel offsets.
+ * @see https://github.com/google/skia/blob/main/gm/fontcache.cpp
+ */
+class FontCacheGm : SkiaGm {
+    override val name = "fontcache"
+    override val renderFamily = RenderFamily.TEXT
+    override val minSimilarity = 0.0
+    override val width = 1280
+    override val height = 1280
+
+    private val typeface = Typefaces.fromResource("fonts/LiberationSans-Regular.ttf")!!
+
+    override fun draw(canvas: GmCanvas, width0: Int, height0: Int) {
+        val sizes = intArrayOf(8, 9, 10, 11, 12, 13, 18, 20, 25)
+        val texts = arrayOf(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "abcdefghijklmnopqrstuvwxyz",
+            "0123456789",
+            "!@#\$%^&*()<>[]{}",
+        )
+
+        val kSize = 1280f
+        val kSubPixelInc = 0.5f
+        val paint = Paint()
+        var x = 0f
+        var y = 10f
+        var subpixelX = 0f
+        var subpixelY = 0f
+        var offsetX = true
+
+        while (true) {
+            for (s in sizes) {
+                val size = (2 * s).toFloat()
+                val font = Font(typeface, size = size, subpixel = true)
+                for (tfi in 0 until 6) {
+                    for (text in texts) {
+                        canvas.drawString(text, x + subpixelX, y + subpixelY, font, paint)
+                        x = s.toFloat() + x + subpixelX + font.measureText(text)
+                        x = kotlin.math.ceil(x)
+                        if (x + 100f > kSize) {
+                            x = 0f
+                            y += kotlin.math.ceil(size + 3f)
+                            if (y > kSize) return
+                        }
+                    }
+                }
+                if (offsetX) subpixelX += kSubPixelInc
+                else subpixelY += kSubPixelInc
+                offsetX = !offsetX
+            }
+        }
+    }
+}
---
+++ integration-tests/skia/src/test/kotlin/org/graphiks/kanvas/skia/gm/text/FontRegenGm.kt
@@ -0,0 +1,38 @@
+package org.graphiks.kanvas.skia.gm.text
+
+import org.graphiks.kanvas.paint.Paint
+import org.graphiks.kanvas.skia.GmCanvas
+import org.graphiks.kanvas.skia.RenderFamily
+import org.graphiks.kanvas.skia.SkiaGm
+import org.graphiks.kanvas.text.Font
+import org.graphiks.kanvas.text.Typefaces
+import org.graphiks.kanvas.types.Color
+
+/**
+ * Port of Skia's `gm/fontregen.cpp::FontRegenGM`.
+ * Draws 4 TextBlobs with different fonts to exercise text blob rendering.
+ * @see https://github.com/google/skia/blob/main/gm/fontregen.cpp
+ */
+class FontRegenGm : SkiaGm {
+    override val name = "fontregen"
+    override val renderFamily = RenderFamily.TEXT
+    override val minSimilarity = 0.0
+    override val width = 512
+    override val height = 512
+
+    private val typeface = Typefaces.fromResource("fonts/LiberationSans-Regular.ttf")!!
+
+    override fun draw(canvas: GmCanvas, width0: Int, height0: Int) {
+        val font80 = Font(typeface, size = 80f, subpixel = false)
+        val font162 = Font(typeface, size = 162f, subpixel = false)
+
+        val b0 = font80.toTextBlob("abcdefghijklmnopqrstuvwxyz", 0f, 0f)
+        val b1 = font162.toTextBlob("ABCDEFGHI", 0f, 0f)
+        val b2 = font162.toTextBlob("NOPQRSTUV", 0f, 0f)
+
+        canvas.drawTextBlob(b0, 10f, 80f, Paint(color = Color.BLACK))
+        canvas.drawTextBlob(b1, 10f, 225f, Paint(color = Color.BLACK))
+        canvas.drawTextBlob(b0, 10f, 305f, Paint(color = Color(0xFF010101u)))
+        canvas.drawTextBlob(b2, 10f, 465f, Paint(color = Color(0xFF010101u)))
+    }
+}
